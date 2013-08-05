92aae963b754c9744fffa0063069fefd03cec4b7
tinyms <i@tinyms.com>
2013-08-05 17:11:10 +0800

    ajax and api

--- tinyms/web.py
+++ tinyms/web.py
@@ -24,60 +24,67 @@ class ApiHandler(IRequest):
         ver = self.get_argument("ver");
         self.set_header("Content-Type","text/json;charset=utf-8")
         if not class_full_name:
-            return
-        obj = Plugin.get(IApi,class_full_name)
-        if not obj:
-            return
-        if hasattr(obj,method_name):
-            func = obj.__getattribute__(method_name)
-            func_params = json.loads(self.get_argument("dict_params"))
-            obj.request(self)
-            result = func(**func_params)
-            self.write(json.dumps(result))
+            self.write("Class Name Require.")
+        else:
+            obj = Plugin.get(IApi,class_full_name)
+            if not obj:
+                self.write("Class Not Found.")
+            else:
+                if hasattr(obj,method_name):
+                    func = obj.__getattribute__(method_name)
+                    func_params = json.loads(self.get_argument("dict_params"))
+                    obj.request(self)
+                    result = func(**func_params)
+                    self.write(json.dumps(result))
+                else:
+                    self.write("The method `%s` not found." % method_name)
 
 class AjaxHandler(IRequest):
 
     def get(self,class_full_name):
         self.set_header("Content-Type","text/javascript;charset=utf-8")
         if not class_full_name:
-            return
-        obj = Plugin.get(IAjax,class_full_name)
-        if not obj:
-            return
-        if hasattr(obj,"__export__"):
-            if len(obj.__export__) > 0:
-                if not hasattr(obj,"client_javascript_object_name"):
-                    print("client javascript object name not assign.")
-                    return
-                client_js_object_name = obj.client_javascript_object_name()
-                js_ = """
-                    %s.%s=function(dict_params, func, data_type){
-                        var p_ = {};
-                        p_.__func_dict_params__ = JSON.stringify(dict_params);
-                        p_.__func_name__ = "%s";
-                        p_.__data_type__ = data_type;
-                        var req = $.ajax({
-                            url: "/ajax/%s.js",type:"POST",data:p_,dataType:data_type
-                        });
-                        req.done(function(data){func(true,data);});
-                        req.fail(function(jqXHR, textStatus) {
-                            alert( "Request failed: " + textStatus );
-                            func(false,textStatus);
-                        })
-                    };
-                """
-                js_buffer = list()
-                js_buffer.append("var %s = {};" % client_js_object_name)
-                for export_func in obj.__export__:
-                    js_buffer.append(js_ % (client_js_object_name,
-                                            export_func,
-                                            export_func,
-                                            class_full_name))
-                    self.write("".join(js_buffer))
+            self.write("alert('Class Name Require.')")
         else:
-            self.write("console.log('Attr `__export__` Not exist');")
+            obj = Plugin.get(IAjax,class_full_name)
+            if not obj:
+                self.write("alert('Class Not Found.')")
+            else:
+                if hasattr(obj,"__export__"):
+                    if len(obj.__export__) > 0:
+                        if not hasattr(obj,"client_javascript_object_name"):
+                            print("client javascript object name not assign.")
+                            return
+                        client_js_object_name = obj.client_javascript_object_name()
+                        js_ = """
+                            %s.%s=function(dict_params, func, data_type){
+                                var p_ = {};
+                                p_.__func_dict_params__ = JSON.stringify(dict_params);
+                                p_.__func_name__ = "%s";
+                                p_.__data_type__ = data_type;
+                                var req = $.ajax({
+                                    url: "/ajax/%s.js",type:"POST",data:p_,dataType:data_type
+                                });
+                                req.done(function(data){func(true,data);});
+                                req.fail(function(jqXHR, textStatus) {
+                                    alert( "Request failed: " + textStatus );
+                                    func(false,textStatus);
+                                })
+                            };
+                        """
+                        js_buffer = list()
+                        js_buffer.append("var %s = {};" % client_js_object_name)
+                        for export_func in obj.__export__:
+                            js_buffer.append(js_ % (client_js_object_name,
+                                                    export_func,
+                                                    export_func,
+                                                    class_full_name))
+                            self.write("".join(js_buffer))
+                else:
+                    self.write("console.log('Attr `__export__` Not exist');")
 
     def post(self,class_full_name):
+
         func_return_data_type = self.get_argument("__data_type__")
         if func_return_data_type == "json":
             self.set_header("Content-Type","text/json;charset=utf-8")
@@ -85,15 +92,18 @@ class AjaxHandler(IRequest):
             self.set_header("Content-Type","text/javascript;charset=utf-8")
         elif func_return_data_type == "html":
             self.set_header("Content-Type","text/html;charset=utf-8")
+
         if not class_full_name:
-            return
-        obj = Plugin.get(IAjax,class_full_name)
-        if not obj:
-            return
-        func_name = self.get_argument("__func_name__")
-        func_params = json.loads(self.get_argument("__func_dict_params__"))
-        if hasattr(obj,func_name):
-            obj.request(self)
-            func = obj.__getattribute__(func_name)
-            result = func(**func_params)
-            self.write(result)
+            self.write("Class Name Require.")
+        else:
+            obj = Plugin.get(IAjax,class_full_name)
+            if not obj:
+                self.write("Class Not Found.")
+            else:
+                func_name = self.get_argument("__func_name__")
+                func_params = json.loads(self.get_argument("__func_dict_params__"))
+                if hasattr(obj,func_name):
+                    obj.request(self)
+                    func = obj.__getattribute__(func_name)
+                    result = func(**func_params)
+                    self.write(result)
