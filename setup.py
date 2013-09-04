146688bfbf331632a2fc15bf2b74bc38f61e7127
tinyms <tinyms.team@gmail.com>
2013-09-04 23:16:39 +0800

    DataTable CURD

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version="1.1",
     description="WaterPress for football match game",
     options={"build_exe": {"includes": ["psycopg2._psycopg","lottery.parse"
-        ,"tinyms.orm","sqlalchemy.dialects.sqlite","sqlalchemy.dialects.postgresql"]}},
+        ,"tinyms.core.orm","sqlalchemy.dialects.sqlite","sqlalchemy.dialects.postgresql"]}},
     executables=[Executable(script="ArchiveX.py",
                             targetName="ArchiveX.exe",
                             icon= "static/images/web_card.ico",
