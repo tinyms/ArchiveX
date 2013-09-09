418a545127e6acaf52ef46f751b007938fe4e233
tinyms <tinyms.team@gmail.com>
2013-09-09 20:28:47 +0800

    theme combine

--- tinyms/core/point.py
+++ tinyms/core/point.py
@@ -3,20 +3,25 @@ __author__ = 'tinyms'
 from functools import wraps
 from tinyms.core.entity import SecurityPoint
 #for plugin to extends
-class EmptyClass():pass
+class EmptyClass(): pass
+
 
 class ObjectPool():
     points = list()
     api = dict()
     ajax = dict()
     url_patterns = list()
+    sidebar_menus = list()
     ui_mapping = dict()
+
 
 class IWebConfig():
     def get_server_port(self):
         return 80
+
     def get_database_driver(self):
         return None
+
     def settings(self, ws_setting):
         """
         Append or modify tornado setting
@@ -25,7 +30,8 @@ class IWebConfig():
         """
         return
 
-def reg(key,category="",group_="",description=""):
+
+def reg(key, category="", group_="", description=""):
     if not key:
         return
     for sp in ObjectPool.points:
@@ -38,19 +44,38 @@ def reg(key,category="",group_="",description=""):
     point.category = category
     ObjectPool.points.append(point)
 
+
 def route(pattern):
     """
     url mapping.
     """
+
     def ref_pattern(cls):
         ObjectPool.url_patterns.append((pattern, cls))
         return cls
+
     return ref_pattern
+
+
+def sidebar(path, label="", position=0):
+    """
+    url mapping.
+    """
+
+    def ref_sidebar(cls):
+        if path:
+            if not label:
+                label_ = path.split("/")[-1]
+            ObjectPool.sidebar_menus.append((path, label_, position))
+        return cls
+
+    return ref_sidebar
+
 
 def auth(points=set()):
     def handle_func(func):
         @wraps(func)
-        def returned_func(*args,**kwargs):
+        def returned_func(*args, **kwargs):
             self_ = args[0]
             account_id = self_.request.get_current_user()
             if not account_id:
@@ -61,34 +86,42 @@ def auth(points=set()):
                 print(True)
             else:
                 print(False)
-            return func(*args,**kwargs)
+            return func(*args, **kwargs)
+
         return returned_func
+
     return handle_func
+
 
 def api(key):
     """
     api mapping.
     """
+
     def ref(cls):
         ObjectPool.api[key] = cls
         return cls
 
     return ref
 
+
 def ajax(key):
     """
     ajax mapping.
     """
+
     def ref(cls):
         ObjectPool.ajax[key] = cls
         return cls
 
     return ref
 
+
 def ui(name):
     """
     ui mapping. 配置UI至模版可用
     """
+
     def ref_pattern(cls):
         ObjectPool.ui_mapping[name] = cls
         return cls
