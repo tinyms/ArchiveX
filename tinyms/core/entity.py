d585acb3e6a6fe635d342e84f58ce723864107a2
tinyms <i@tinyms.com>
2013-09-25 16:31:39 +0800

    ..

--- tinyms/core/entity.py
+++ tinyms/core/entity.py
@@ -1,19 +1,75 @@
 __author__ = 'tinyms'
 
-from sqlalchemy import Column, Integer, String, DateTime, Text, Date, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Text, Date, Boolean, Numeric
 from tinyms.core.orm import Entity, Simplify, many_to_one, many_to_many
 
 
 class Archives(Entity, Simplify):
+    #姓名
     name = Column(String(20), nullable=False)
-    alias = Column(String(10))
-    sex = Column(Integer(1))
+    #别名、曾用名
+    alias = Column(String(20))
+    #性别
+    sex = Column(String(5))
+    #生日
     birthday = Column(Date())
-    email = Column(String(65),unique=True)
+    #体重(千克)
+    weight = Column(Numeric(2, 2))
+    #身高(厘米)
+    height = Column(Numeric(2, 2))
+    #民族
+    nation = Column(String(65))
+    #籍贯
+    hometown = Column(String(10))
+    #视力
+    vision = Column(Numeric(2, 2))
+    #职位
+    post = Column(Integer())
+    #健康状况
+    health_state = Column(String(20))
+    #婚姻状况
+    marital_status = Column(Boolean())
+    #身份证
+    id_card = Column(String(20))
+    #户籍所在地
+    household_register = Column(String(100))
+    #学历
+    educational_background = Column(Integer())
+    #专业
+    specialty = Column(Integer())
+    #毕业院校
+    last_leave_school = Column(String(30))
+    #政治面貌
+    political_landscape = Column(Integer())
+    #职称等级
+    profession_level = Column(String(30))
+    #爱好
+    hobby = Column(String(60))
+    #性格
+    disposition = Column(String(60))
+    #特长
+    strong_point = Column(String(60))
+    #综合技能
+    comple_skills = Column(String(60))
+    #现住址
+    current_address = Column(String(250))
+    #现住址电话
+    current_address_phone = Column(String(60))
+    #录用途径
+    employment_pathways = Column(Integer())
+    #介绍人
+    employment_referrals = Column(String(10))
+    #紧急联系人
+    emergency_contact = Column(String(10))
+    #紧急联系人电话
+    emergency_contact_phone = Column(String(20))
+    #邮箱
+    email = Column(String(65), unique=True)
+    #移动电话
     mobile_phone = Column(String(20))
     #accounts
 
-
+#档案
 @many_to_one("Archives")
 class Account(Entity, Simplify):
     login_name = Column(String(20), unique=True, nullable=False)
@@ -24,7 +80,7 @@ class Account(Entity, Simplify):
     #archives
     #roles
 
-
+#账户
 @many_to_many("Account")
 class Role(Entity, Simplify):
     name = Column(String(60), unique=True, nullable=False)
@@ -32,7 +88,7 @@ class Role(Entity, Simplify):
     #securitypoints
     #accounts
 
-
+#安全点
 @many_to_many("Role")
 class SecurityPoint(Entity, Simplify):
     key_ = Column(String(60), unique=True, nullable=False)
@@ -41,15 +97,17 @@ class SecurityPoint(Entity, Simplify):
     category = Column(String(60), nullable=False)
     #roles
 
-class JobTitle(Entity,Simplify):
+#职位、头衔
+class JobTitle(Entity, Simplify):
     name = Column(String(60), unique=True, nullable=False)
 
+#分类字典
 class Term(Entity, Simplify):
     name = Column(String(20), unique=True, nullable=False)
     slug = Column(String(20), nullable=False)
     #termtaxonomys
 
-
+#分类
 @many_to_one("Term")
 @many_to_one("TermTaxonomy")
 class TermTaxonomy(Entity, Simplify):
