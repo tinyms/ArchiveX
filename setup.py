e084de4731568a36cb0537dc49fc6fdf3013f016
tinyms <i@tinyms.com>
2013-10-29 17:59:32 +0800

    fix setting page

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ __author__ = 'tinyms'
 
 #import sys
 from cx_Freeze import setup, Executable
+
 base = None
 # if sys.platform == "win32":
 #     base = "Win32GUI"
@@ -10,9 +11,9 @@ setup(
     name="ArchiveX",
     version="1.1",
     description="WaterPress for football match game",
-    options={"build_exe": {"includes": ["psycopg2._psycopg","lottery.parse"
-        ,"tinyms.core.orm","sqlalchemy.dialects.sqlite","sqlalchemy.dialects.postgresql"]}},
+    options={"build_exe": {"includes": ["psycopg2._psycopg", "lottery.parse", "tinyms.core.orm",
+                                        "sqlalchemy.dialects.sqlite", "sqlalchemy.dialects.postgresql"]}},
     executables=[Executable(script="ArchiveX.py",
                             targetName="ArchiveX.exe",
-                            icon= "static/images/web_card.ico",
+                            icon="static/images/web_card.ico",
                             base=base)])
